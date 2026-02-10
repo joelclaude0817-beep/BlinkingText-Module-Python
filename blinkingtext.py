7afb1e4c2870881eedcfccac116d39ecbfd8b3c6
Joel <joelclaude0817@gmail.com>
2026-02-10 14:47:34 +0800

    Add blinking text functionality in blinkingtext.py

---
+++ blinkingtext.py
@@ -0,0 +1,12 @@
+import os
+import time
+
+def blinkingtext():
+    while True:
+        print("Text")
+        time.sleep(0.5)
+        os.system("clear")
+        print("        ")
+        time.sleep(0.5)
+        os.system("clear")
+# This is a custom module.
